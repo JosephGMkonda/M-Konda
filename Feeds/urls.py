7b41955f8a0258f4e06872717c83d68ed8347d22
sangwaniKondowe <josephgmkonda@gmail.com>
2022-09-17 11:32:31 -0800

    likes done

--- Feeds/urls.py
+++ Feeds/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.feed, name='feeds'),
     path('add-post',views.addPost, name='add-post'),
     path('<uuid:post_id>',views.postDetails, name='postdetail'),
-    path('<uuid:post_like/like', views.Likes, name='likes')
+    path('<uuid:post_id>/like', views.Likes, name='likes')
+
+    # path('<uuid:post_id>/like',like, name='postlike')
 ]
